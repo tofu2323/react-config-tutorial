030d6c648010ed2caff71f967367d2b41cf43f10
Fumiya Goto <gotch@styz.io>
2019-05-01 09:46:43 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    // path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    // contentBase: './dist',
+    contentBase: './build',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html'),
+    }),
+  ],
+};
